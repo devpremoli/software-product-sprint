ee287770245d7a99714391e8aa29cf6506b044ea
Prem20560404 <prem.oli@bison.howard.edu>
2022-06-13 15:02:54 +0000

    add and style contact form and servlet file

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -13,12 +13,18 @@ public class FormHandlerServlet extends HttpServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
     // Get the value entered in the form.
-    String textValue = request.getParameter("text-input");
+    String message = request.getParameter("text-input");
+    String email = request.getParameter("email");
+    String name = request.getParameter("name");
 
     // Print the value so you can see it in the server logs.
-    System.out.println("You submitted: " + textValue);
+    System.out.println("Your name: " + name);
+    System.out.println("You email: " + email);
+    System.out.println("You message: " + message);
 
     // Write the value to the response so the user can see it.
-    response.getWriter().println("You submitted: " + textValue);
+    response.getWriter().println("You name: " + name);
+    response.getWriter().println("You email: " + email);
+    response.getWriter().println("You submitted: " + message);
   }
 }
