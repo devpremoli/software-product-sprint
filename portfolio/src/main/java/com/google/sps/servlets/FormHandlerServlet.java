01d765bbb44982149e79aab2b7e78cb74f77783e
Prem20560404 <prem.oli@bison.howard.edu>
2022-06-18 11:25:38 +0000

    add servlet files

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -28,3 +28,55 @@ public class FormHandlerServlet extends HttpServlet {
     response.getWriter().println("Your message: " + message);
   }
 }
+
+
+/**
+ * Takes an image submitted by the user and uploads it to Cloud Storage, and then displays it as
+ * HTML in the response.
+ */
+@WebServlet("/upload")
+@MultipartConfig
+public class FormHandlerServlet extends HttpServlet {
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+    // Get the message entered by the user.
+    String message = request.getParameter("message");
+
+    // Get the file chosen by the user.
+    Part filePart = request.getPart("image");
+    String fileName = filePart.getSubmittedFileName();
+    InputStream fileInputStream = filePart.getInputStream();
+
+    // Upload the file and get its URL
+    String uploadedFileUrl = uploadToCloudStorage(fileName, fileInputStream);
+
+    // Output some HTML that shows the data the user entered.
+    // You could also store the uploadedFileUrl in Datastore instead.
+    PrintWriter out = response.getWriter();
+    out.println("<p>Here's the image you uploaded:</p>");
+    out.println("<a href=\"" + uploadedFileUrl + "\">");
+    out.println("<img src=\"" + uploadedFileUrl + "\" />");
+    out.println("</a>");
+    out.println("<p>Here's the text you entered:</p>");
+    out.println(message);
+  }
+
+  /** Uploads a file to Cloud Storage and returns the uploaded file's URL. */
+  private static String uploadToCloudStorage(String fileName, InputStream fileInputStream) {
+    String projectId = "poli-sps-summer22";
+    String bucketName = "poli-sps-summer22.appspot.com";
+    Storage storage =
+        StorageOptions.newBuilder().setProjectId(projectId).build().getService();
+    BlobId blobId = BlobId.of(bucketName, fileName);
+    BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
+
+    // Upload the file to Cloud Storage.
+    Blob blob = storage.create(blobInfo, fileInputStream);
+
+    // Return the uploaded file's URL.
+    return blob.getMediaLink();
+  }
+}
